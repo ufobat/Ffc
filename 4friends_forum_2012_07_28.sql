bea4b648db5493122ee8c89eed8233b1461d42d0
Bedivere <peters_mops@arcor.de>
2012-07-28 01:18:59 +0200

    überarbeiten für neue datenbankstruktur

---
+++ 4friends_forum_2012_07_28.sql
@@ -0,0 +1,259 @@
+-- 
+-- Structure for table `phpbb_attachments`
+-- 
+
+DROP TABLE IF EXISTS `phpbb_attachments`;
+CREATE TABLE IF NOT EXISTS `phpbb_attachments` (
+  `attach_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `post_msg_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `topic_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `in_message` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `poster_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `physical_filename` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `real_filename` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `filesize` int(20) unsigned NOT NULL DEFAULT '0',
+  `filetime` int(11) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`attach_id`),
+  KEY `filetime` (`filetime`),
+  KEY `post_msg_id` (`post_msg_id`),
+  KEY `topic_id` (`topic_id`),
+  KEY `poster_id` (`poster_id`),
+) ENGINE=MyISAM AUTO_INCREMENT=159 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- 
+-- Structure for table `phpbb_forums`
+-- 
+
+DROP TABLE IF EXISTS `phpbb_forums`;
+CREATE TABLE IF NOT EXISTS `phpbb_forums` (
+  `forum_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `forum_name` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `forum_desc` text COLLATE utf8_bin NOT NULL,
+  `forum_last_post_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `forum_last_poster_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`forum_id`),
+  KEY `forum_lastpost_id` (`forum_last_post_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- 
+-- Structure for table `phpbb_posts`
+-- 
+
+DROP TABLE IF EXISTS `phpbb_posts`;
+CREATE TABLE IF NOT EXISTS `phpbb_posts` (
+  `post_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `topic_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `forum_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `poster_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `icon_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `poster_ip` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `post_time` int(11) unsigned NOT NULL DEFAULT '0',
+  `post_subject` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `post_text` mediumtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `post_attachment` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`post_id`),
+  KEY `forum_id` (`forum_id`),
+  KEY `topic_id` (`topic_id`),
+  KEY `poster_ip` (`poster_ip`),
+  KEY `poster_id` (`poster_id`),
+  KEY `tid_post_time` (`topic_id`,`post_time`),
+  FULLTEXT KEY `post_subject` (`post_subject`),
+  FULLTEXT KEY `post_text` (`post_text`),
+  FULLTEXT KEY `post_content` (`post_subject`,`post_text`)
+) ENGINE=MyISAM AUTO_INCREMENT=23372 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- 
+-- Structure for table `phpbb_privmsgs`
+-- 
+
+DROP TABLE IF EXISTS `phpbb_privmsgs`;
+CREATE TABLE IF NOT EXISTS `phpbb_privmsgs` (
+  `msg_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `root_level` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `author_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `icon_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `author_ip` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `message_time` int(11) unsigned NOT NULL DEFAULT '0',
+  `enable_bbcode` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `enable_smilies` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `enable_magic_url` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `enable_sig` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `message_subject` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `message_text` mediumtext COLLATE utf8_bin NOT NULL,
+  `message_edit_reason` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `message_edit_user` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `message_attachment` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `bbcode_bitfield` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `bbcode_uid` varchar(8) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `message_edit_time` int(11) unsigned NOT NULL DEFAULT '0',
+  `message_edit_count` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `to_address` text COLLATE utf8_bin NOT NULL,
+  `bcc_address` text COLLATE utf8_bin NOT NULL,
+  `message_reported` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`msg_id`),
+  KEY `author_ip` (`author_ip`),
+  KEY `message_time` (`message_time`),
+  KEY `author_id` (`author_id`),
+  KEY `root_level` (`root_level`)
+) ENGINE=MyISAM AUTO_INCREMENT=3285 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- 
+-- Structure for table `phpbb_privmsgs_to`
+-- 
+
+DROP TABLE IF EXISTS `phpbb_privmsgs_to`;
+CREATE TABLE IF NOT EXISTS `phpbb_privmsgs_to` (
+  `msg_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `user_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `author_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `pm_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `pm_new` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `pm_unread` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `pm_replied` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `pm_marked` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `pm_forwarded` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `folder_id` int(11) NOT NULL DEFAULT '0',
+  KEY `msg_id` (`msg_id`),
+  KEY `author_id` (`author_id`),
+  KEY `usr_flder_id` (`user_id`,`folder_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- 
+-- Structure for table `phpbb_topics`
+-- 
+
+DROP TABLE IF EXISTS `phpbb_topics`;
+CREATE TABLE IF NOT EXISTS `phpbb_topics` (
+  `topic_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `forum_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `icon_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `topic_attachment` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `topic_approved` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `topic_reported` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `topic_title` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `topic_poster` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `topic_time` int(11) unsigned NOT NULL DEFAULT '0',
+  `topic_time_limit` int(11) unsigned NOT NULL DEFAULT '0',
+  `topic_views` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `topic_replies` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `topic_replies_real` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `topic_status` tinyint(3) NOT NULL DEFAULT '0',
+  `topic_type` tinyint(3) NOT NULL DEFAULT '0',
+  `topic_first_post_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `topic_first_poster_name` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `topic_first_poster_colour` varchar(6) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `topic_last_post_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `topic_last_poster_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `topic_last_poster_name` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `topic_last_poster_colour` varchar(6) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `topic_last_post_subject` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `topic_last_post_time` int(11) unsigned NOT NULL DEFAULT '0',
+  `topic_last_view_time` int(11) unsigned NOT NULL DEFAULT '0',
+  `topic_moved_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `topic_bumped` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `topic_bumper` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `poll_title` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `poll_start` int(11) unsigned NOT NULL DEFAULT '0',
+  `poll_length` int(11) unsigned NOT NULL DEFAULT '0',
+  `poll_max_options` tinyint(4) NOT NULL DEFAULT '1',
+  `poll_last_vote` int(11) unsigned NOT NULL DEFAULT '0',
+  `poll_vote_change` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`topic_id`),
+  KEY `forum_id` (`forum_id`),
+  KEY `forum_id_type` (`forum_id`,`topic_type`),
+  KEY `last_post_time` (`topic_last_post_time`),
+  KEY `topic_approved` (`topic_approved`),
+  KEY `forum_appr_last` (`forum_id`,`topic_approved`,`topic_last_post_id`),
+  KEY `fid_time_moved` (`forum_id`,`topic_last_post_time`,`topic_moved_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=865 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- 
+-- Structure for table `phpbb_users`
+-- 
+
+DROP TABLE IF EXISTS `phpbb_users`;
+CREATE TABLE IF NOT EXISTS `phpbb_users` (
+  `user_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `user_type` tinyint(2) NOT NULL DEFAULT '0',
+  `group_id` mediumint(8) unsigned NOT NULL DEFAULT '3',
+  `user_permissions` mediumtext COLLATE utf8_bin NOT NULL,
+  `user_perm_from` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `user_ip` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_regdate` int(11) unsigned NOT NULL DEFAULT '0',
+  `username` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `username_clean` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_password` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_passchg` int(11) unsigned NOT NULL DEFAULT '0',
+  `user_pass_convert` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `user_email` varchar(100) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_email_hash` bigint(20) NOT NULL DEFAULT '0',
+  `user_birthday` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_lastvisit` int(11) unsigned NOT NULL DEFAULT '0',
+  `user_lastmark` int(11) unsigned NOT NULL DEFAULT '0',
+  `user_lastpost_time` int(11) unsigned NOT NULL DEFAULT '0',
+  `user_lastpage` varchar(200) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_last_confirm_key` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_last_search` int(11) unsigned NOT NULL DEFAULT '0',
+  `user_warnings` tinyint(4) NOT NULL DEFAULT '0',
+  `user_last_warning` int(11) unsigned NOT NULL DEFAULT '0',
+  `user_login_attempts` tinyint(4) NOT NULL DEFAULT '0',
+  `user_inactive_reason` tinyint(2) NOT NULL DEFAULT '0',
+  `user_inactive_time` int(11) unsigned NOT NULL DEFAULT '0',
+  `user_posts` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `user_lang` varchar(30) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_timezone` decimal(5,2) NOT NULL DEFAULT '0.00',
+  `user_dst` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `user_dateformat` varchar(30) COLLATE utf8_bin NOT NULL DEFAULT 'd M Y H:i',
+  `user_style` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `user_rank` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `user_colour` varchar(6) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_new_privmsg` int(4) NOT NULL DEFAULT '0',
+  `user_unread_privmsg` int(4) NOT NULL DEFAULT '0',
+  `user_last_privmsg` int(11) unsigned NOT NULL DEFAULT '0',
+  `user_message_rules` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `user_full_folder` int(11) NOT NULL DEFAULT '-3',
+  `user_emailtime` int(11) unsigned NOT NULL DEFAULT '0',
+  `user_topic_show_days` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `user_topic_sortby_type` varchar(1) COLLATE utf8_bin NOT NULL DEFAULT 't',
+  `user_topic_sortby_dir` varchar(1) COLLATE utf8_bin NOT NULL DEFAULT 'd',
+  `user_post_show_days` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `user_post_sortby_type` varchar(1) COLLATE utf8_bin NOT NULL DEFAULT 't',
+  `user_post_sortby_dir` varchar(1) COLLATE utf8_bin NOT NULL DEFAULT 'a',
+  `user_notify` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `user_notify_pm` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `user_notify_type` tinyint(4) NOT NULL DEFAULT '0',
+  `user_allow_pm` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `user_allow_viewonline` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `user_allow_viewemail` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `user_allow_massemail` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `user_options` int(11) unsigned NOT NULL DEFAULT '230271',
+  `user_avatar` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_avatar_type` tinyint(2) NOT NULL DEFAULT '0',
+  `user_avatar_width` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `user_avatar_height` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `user_sig` mediumtext COLLATE utf8_bin NOT NULL,
+  `user_sig_bbcode_uid` varchar(8) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_sig_bbcode_bitfield` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_from` varchar(100) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_icq` varchar(15) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_aim` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_yim` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_msnm` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_jabber` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_website` varchar(200) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_occ` text COLLATE utf8_bin NOT NULL,
+  `user_interests` text COLLATE utf8_bin NOT NULL,
+  `user_actkey` varchar(32) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_newpasswd` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_form_salt` varchar(32) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_new` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `user_reminded` tinyint(4) NOT NULL DEFAULT '0',
+  `user_reminded_time` int(11) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username_clean` (`username_clean`),
+  KEY `user_birthday` (`user_birthday`),
+  KEY `user_email_hash` (`user_email_hash`),
+  KEY `user_type` (`user_type`)
+) ENGINE=MyISAM AUTO_INCREMENT=104 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
