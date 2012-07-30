eaeef296f0bb8ad4a94c145d1feaf82608602934
Markus Pinkert <markus.pinkert@arcormail.de>
2012-07-30 20:26:34 +0200

    style angepasst

--- lib/AltSimpleBoard.pm
+++ lib/AltSimpleBoard.pm
@@ -18,6 +18,8 @@ sub startup {
     $r->route('/login')->to('auth#login')->name('login');
     my $b = $r->bridge()->to('auth#check_login');
     $b->route('/board')->to('board#frontpage')->name('frontpage');
+    $b->route('/msg')->to('msg#list')->name('msglist');
+    $b->route('/msg/:to', to => qr(\w{2,32}))->to('msg#userlist')->name('usermsg');
 }
 
 1;
