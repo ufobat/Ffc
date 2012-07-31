a233ded647c15453735ccadb5ac0381e37bf1d64
Markus Pinkert <markus.pinkert@arcormail.de>
2012-07-31 17:37:05 +0200

    jetzt fehlt noch die avatardurchleitung

--- lib/AltSimpleBoard/Data/Board.pm
+++ lib/AltSimpleBoard/Data/Board.pm
@@ -8,14 +8,14 @@ use AltSimpleBoard::Data;
 
 sub get_posts {
     my $data = AltSimpleBoard::Data::dbh()
-      ->selectall_arrayref( 'SELECT id, user, time, text FROM '.$AltSimpleBoard::Data::Prefix.'posts'
+      ->selectall_arrayref( 'SELECT id, user, time, text, avatar FROM '.$AltSimpleBoard::Data::Prefix.'posts'
         , undef );
     for my $p ( @$data ) {
         my @t = localtime $p->[2];
         $t[5] += 1900; $t[4]++;
         $p->[2] = sprintf '%d.%d.%d, %d:%02d', @t[3,4,5,2,1];
         $p->[3] =~ s{\n}{</p>\n<p>}gsm;
-        $p->[3] = "<p>$p->[3]</p>"
+        $p->[3] = "<p>$p->[3]</p>";
     }
     return $data;
 }
