22d7d587b40fff41ea9648c8374e5312ec94d21d
Markus Pinkert <markus.pinkert@arcormail.de>
2012-08-02 11:24:31 +0200

    sessionhandling verbessert

--- lib/AltSimpleBoard.pm
+++ lib/AltSimpleBoard.pm
@@ -15,8 +15,8 @@ sub startup {
     # Normal route to controller
     $r->route('/logout')->to('auth#logout')->name('logout');
     $r->route('/login')->to('auth#login')->name('login');
+    $r->route('/')->to('board#startpage');
     my $b = $r->bridge()->to('auth#check_login');
-    $b->route('/')->to('board#frontpage');
     $b->route('/board')->to('board#frontpage')->name('frontpage');
     $b->route('/post/:id/edit', id => qr(\d+))->to('board#frontpage')->name('editpost');
     $b->route('/msg')->to('msg#list')->name('msglist');
--- lib/AltSimpleBoard/Board.pm
+++ lib/AltSimpleBoard/Board.pm
@@ -2,10 +2,21 @@ package AltSimpleBoard::Board;
 use Mojo::Base 'Mojolicious::Controller';
 use utf8;
 use AltSimpleBoard::Data::Board;
+use AltSimpleBoard::Auth;
 
 sub frontpage {
     my $c = shift;
     $c->stash( posts => AltSimpleBoard::Data::Board::get_posts() );
+}
+
+sub startpage {
+    my $c = shift;
+    if ( AltSimpleBoard::Auth::check_login_status($c) ) {
+        $c->redirect_to('frontpage');
+    }
+    else {
+        $c->render( 'auth/login_form', error => 'Bitte melden Sie sich an' );
+    }
 }
 
 1;
