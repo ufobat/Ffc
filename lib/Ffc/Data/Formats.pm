118c5d99a7ea7d11fdfc68d00e4693e5472adcf2
Bedivere <peters_mops@arcor.de>
2013-05-28 12:58:36 +0200

    altes style entfernt, template- (dummy-) style eingefügt

--- lib/Ffc/Data/Formats.pm
+++ lib/Ffc/Data/Formats.pm
@@ -110,7 +110,7 @@ sub _make_smiley {
     $y =~ s/\>/&gt;/xmsg;
     $y =~ s/\</&lt;/xmsg;
     my $ext = 'png';
-    $ext = 'svg' if $Smiley{$x} eq 'smile';
+#    $ext = 'svg' if $Smiley{$x} eq 'smile';
     return qq~$s<img class="smiley" src="~
         . $c->url_for("$Ffc::Data::Themedir/".$c->session()->{theme}."/img/smileys/$Smiley{$x}.$ext")
         . qq~" alt="$y" />~;
