3168eb84ed51bcc51296024b21f722b4b89a09d8
Bedivere <peters_mops@arcor.de>
2014-03-27 20:45:43 +0100

    Farben nach Helligkeit sortiert

--- lib/Ffc/Plugin/Config.pm
+++ lib/Ffc/Plugin/Config.pm
@@ -31,27 +31,26 @@ our %FontSizeMap = (
      3, 1.75,
      4, 2,
 );
-our @Colors = qw(Maroon DarkRed FireBrick Red Salmon Tomato Coral OrangeRed
-    Chocolate SandyBrown DarkOrange Orange DarkGoldenrod Goldenrod Gold Olive 
-    Yellow YellowGreen GreenYellow Chartreuse LawnGreen Green Lime LimeGreen 
-    SpringGreen MediumSpringGreen Turquoise LightSeaGreen MediumTurquoise 
-    Teal DarkCyan Aqua Cyan DarkTurquoise DeepSkyBlue DodgerBlue RoyalBlue 
-    Navy DarkBlue MediumBlue Blue BlueViolet DarkOrchid DarkViolet 
-    Purple DarkMagenta Fuchsia Magenta MediumVioletRed DeepPink HotPink Crimson 
-    Brown IndianRed RosyBrown LightCoral Snow MistyRose DarkSalmon 
-    LightSalmon Sienna SeaShell SaddleBrown Peachpuff Peru Linen Bisque 
-    Burlywood Tan AntiqueWhite NavajoWhite BlanchedAlmond PapayaWhip Moccasin 
-    Wheat Oldlace FloralWhite Cornsilk Khaki LemonChiffon PaleGoldenrod 
-    DarkKhaki Beige LightGoldenrodYellow LightYellow Ivory OliveDrab 
-    DarkOliveGreen DarkSeaGreen DarkGreen ForestGreen LightGreen 
-    PaleGreen Honeydew SeaGreen MediumSeaGreen Mintcream 
-    MediumAquamarine Aquamarine DarkSlateGray PaleTurquoise LightCyan Azure 
-    CadetBlue PowderBlue LightBlue SkyBlue LightskyBlue SteelBlue AliceBlue 
-    SlateGray LightSlateGray LightsteelBlue CornflowerBlue Lavender 
-    GhostWhite MidnightBlue SlateBlue DarkSlateBlue MediumSlateBlue 
-    MediumPurple Indigo MediumOrchid Plum Violet Thistle Orchid 
-    LavenderBlush PaleVioletRed Pink LightPink Black DimGray Gray DarkGray 
-    Silver LightGray Gainsboro WhiteSmoke White);
+our @Colors = qw(
+    Black Navy DarkBlue MediumBlue MidnightBlue Indigo Blue Maroon
+    DarkRed Purple DarkMagenta DarkViolet SaddleBrown DarkSlateBlue DarkGreen DarkSlateGray
+    Brown FireBrick Sienna BlueViolet DarkOliveGreen Green Crimson MediumVioletRed
+    Red DarkOrchid Teal DimGray Chocolate Fuchsia Magenta SlateBlue
+    Olive IndianRed DarkCyan OrangeRed RoyalBlue ForestGreen MediumOrchid DeepPink
+    DarkGoldenrod SeaGreen OliveDrab MediumSlateBlue Tomato PaleVioletRed Gray SlateGray
+    Peru DodgerBlue DarkOrange MediumPurple SteelBlue Coral LightSlateGray Orchid
+    Salmon LightCoral HotPink RosyBrown Orange Goldenrod CadetBlue LightSeaGreen
+    DarkSalmon DeepSkyBlue Violet CornflowerBlue MediumSeaGreen SandyBrown DarkTurquoise LightSalmon
+    LimeGreen DarkGray DarkKhaki Plum Tan DarkSeaGreen YellowGreen BurlyWood
+    Gold MediumAquamarine LightPink Silver Lime MediumTurquoise MediumSpringGreen Thistle
+    SpringGreen LightSteelBlue Pink Turquoise Aqua Cyan LawnGreen SkyBlue
+    Chartreuse LightSkyBlue LightGray LightBlue LightGreen GreenYellow PeachPuff Yellow
+    Khaki Wheat NavajoWhite Gainsboro PowderBlue PaleGoldenrod Moccasin Bisque
+    PaleGreen MistyRose PaleTurquoise BlanchedAlmond Lavender AntiqueWhite Aquamarine PapayaWhip
+    Linen Beige LavenderBlush OldLace LemonChiffon LightGoldenrodYellow Cornsilk Seashell
+    WhiteSmoke AliceBlue FloralWhite GhostWhite LightYellow Snow Honeydew LightCyan
+    Ivory MintCream Azure White
+);
 
 sub register {
     my ( $self, $app ) = @_;
