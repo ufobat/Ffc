4a20374f0ffdade10f6f11b67c465641ff1fb395
Bedivere <peters_mops@arcor.de>
2014-03-30 17:34:05 +0200

    routen für die echte Action angelegt

--- lib/Ffc.pm
+++ lib/Ffc.pm
@@ -3,6 +3,12 @@ use strict; use warnings; use utf8;
 use Mojo::Base 'Mojolicious';
 use File::Spec::Functions qw(catdir);
 
+our $Digqr = qr/\d+/xmso;
+our $Usrqr = qr(\w{2,32})xmso;
+our $Catqr = qr(\w{1,64})xmso;
+our $Bgcqr = qr(\w{3,32})xmso;
+our $Fszqr = qr(-?\d{1,3})xmso;
+
 # This method will run once at server start
 sub startup {
     $_[0]->plugin('Ffc::Plugin::Config');
@@ -15,7 +21,7 @@ sub _install_routes {
     my $l = _install_routes_auth($_[0]->routes);
 
     # Standardseitenauslieferungen
-    $l->get('/')->to('board#frontpage')->name('show');
+    $l->any('/')->to('board#frontpage')->name('show');
     $l->get('/session')
       ->to( sub { $_[0]->render( json => $_[0]->session() ) } )
       ->name('sessiondata');
@@ -23,6 +29,87 @@ sub _install_routes {
     # Optionen-Routen
     _install_routes_avatars($l);
     _install_routes_options($l);
+    _install_routes_forum($l);
+    _install_routes_pmsgs($l);
+    _install_routes_notes($l);
+}
+
+sub _install_show_route {
+    $_[0]->get('/show')->to('board#frontpage')->name('show'.($_[1] ? "_$_[1]" : ''));
+    $_[0]->post('/search')->to('board#frontpage')->name('search'.($_[1] ? "_$_[1]" : ''));
+}
+sub _install_basic_post_routes {
+    my ( $r, $n ) = @_;
+    $r->post('/new')
+      ->to('board#new_post')
+      ->name("new_post$n");
+    $r->get('/showpost/:postid', [postid => $Digqr])
+      ->to('board#show_post')
+      ->name("show_post$n");
+}
+sub _install_edit_routes {
+    my $r = $_[0];
+    my $n = $_[1] ? "_$_[1]" : '';
+
+    # Post-Handling
+    _install_basic_post_routes($r, $n);
+    $r->get('/edit/:postid', [postid => $Digqr])
+      ->to('board#edit_post_form')
+      ->name("edit_post_form$n");
+    $r->post('/edit/:postid', [postid => $Digqr])
+      ->to('board#edit_post_do')
+      ->name("edit_post_do$n");
+    $r->get('/delete/:postid', [postid => $Digqr])
+      ->to('board#delete_post_confirm')
+      ->name("delete_post$n");
+    $r->post('/delete/:postid', [postid => $Digqr])
+      ->to('board#delete_post_do')
+      ->name("delete_post$n");
+    
+    # Upload-Handling
+    $r->get('/upload/:postid', [postid => $Digqr])
+      ->to('board#upload_form')
+      ->name("upload_form$n");
+    $r->post('/upload/:postid', [postid => $Digqr])
+      ->to('board#upload')
+      ->name("upload$n");
+    $r->get('/upload/delete/:uploadid', [uploadid => $Digqr])
+      ->to('board#deleteupload_confirm')
+      ->name("delete_upload_confirm$n");
+    $r->post('/upload/delete/:uploadid', [uploadid => $Digqr])
+      ->to('board#delete_upload_do')
+      ->name("delete_upload_do$n");
+}
+sub _install_switch_bridge {
+    my $n = $_[1];
+    my $r = $_[0]->bridge("/$n")->under(sub{$_[0]->stash(act => $n)})->name("switch_${n}_bridge");
+    _install_show_route($r, $_[1]);
+    return $r;
+}
+sub _install_routes_forum {
+    my $n = 'forum';
+    my $r = _install_switch_bridge($_[0], $n);
+    _install_edit_routes($r, $n);
+    my $c = $r->bridge('/cat/:catname', [catname => $Catqr])->name('forum_cat_bridge');
+    _install_show_route($c, 'forum_cat');
+    _install_edit_routes($c, 'forum_cat');
+    for ( [$r,$n], [$c,"${n}_cat"] ) {
+        my ( $r, $n ) = @$_;
+        my $k = $r->bridge('/comment')->name("${n}_comment_bridge");
+        _install_edit_routes($k, "${n}_comment");
+    }
+}
+sub _install_routes_pmsgs {
+    my $n = 'pmsgs';
+    my $r = _install_switch_bridge($_[0], $n)
+                ->bridge('/user/:username', [username => $Usrqr])
+                ->name("${n}_user_bridge");
+    _install_basic_post_routes($r, "_$n");
+}
+sub _install_routes_notes {
+    my $n = 'notes';
+    my $r = _install_switch_bridge($_[0], $n);
+    _install_edit_routes($r, $n);
 }
 
 sub _install_routes_auth {
@@ -36,8 +123,8 @@ sub _install_routes_auth {
 }
 
 sub _install_routes_avatars {
-    my $p = $_[0]->any('/avatar')->name('avatars_bridge');
-    $p->get('/:username', [username => qr(\w{2,32})xmso])
+    my $p = $_[0]->bridge('/avatar')->name('avatars_bridge');
+    $p->get('/:username', [username => $Usrqr])
       ->to('avatars#avatar_show')
       ->name('avatar_show');
     $p->post('/upload')
@@ -46,7 +133,7 @@ sub _install_routes_avatars {
 }
 
 sub _install_routes_options {
-    my $o = $_[0]->any('/options')->name('options_bridge');
+    my $o = $_[0]->bridge('/options')->name('options_bridge');
 
     # Optionsformular
     $o->get('/form')
@@ -57,17 +144,17 @@ sub _install_routes_options {
     $o->get('/switchtheme')
       ->to('options#switch_theme')
       ->name('switch_theme');
-    $o->get('/fontsize/:fontsize', [fontsize => qr(-?\d{1,3})xmso])
+    $o->get('/fontsize/:fontsize', [fontsize => $Fszqr])
       ->to('options#font_size')
       ->name('font_size');
-    my $b = $o->get('/bgcolor')->name('bgcolor_bridge');
+    my $b = $o->bridge('/bgcolor')->name('bgcolor_bridge');
     $b->get('/none')
       ->to('options#no_bg_color')
       ->name('no_bg_color');
-    $b->get('/color/:bgcolor', [bgcolor => qr(\w{3,32})])
+    $b->get('/color/:bgcolor', [bgcolor => $Bgcqr])
       ->to('options#bg_color')
       ->name('bg_color');
-    $o->get('/toggle/cat/:cat', [cat => qr(\w{1,64})])
+    $o->get('/toggle/cat/:cat', [cat => $Catqr])
       ->to('options#toggle_cat')
       ->name('toggle_cat');
 
--- lib/Ffc/Board.pm
+++ lib/Ffc/Board.pm
@@ -4,6 +4,7 @@ use Mojo::Base 'Mojolicious::Controller';
 
 sub frontpage {
     my $c = shift;
+    $c->stash(act => $c->param('act'));
     $c->set_error('Der Text, der erscheint, wenn etwas schief gelaufen ist oder so nicht gemacht werden darf.');
     $c->set_info('Dieser Text weißt den Benutzer auf erfolgreiche Aktionen im System hin.');
     $c->set_warning('Mit diesem Text soll dem Benutzer deutlich gemacht werden, dass etwas im Moment noch nicht so wie erwartet ausgeführt werden konnte und dass der Benutzer ggf. noch etwas korrigieren oder prüfen sollte.');
