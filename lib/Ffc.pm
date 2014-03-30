ae13071e53b0d4f34aa92818d9e0af8fb3c64124
Bedivere <peters_mops@arcor.de>
2014-03-30 17:45:23 +0200

    Subs umsortiert

--- lib/Ffc.pm
+++ lib/Ffc.pm
@@ -34,84 +34,6 @@ sub _install_routes {
     _install_routes_notes($l);
 }
 
-sub _install_show_route {
-    $_[0]->get('/show')->to('board#frontpage')->name('show'.($_[1] ? "_$_[1]" : ''));
-    $_[0]->post('/search')->to('board#frontpage')->name('search'.($_[1] ? "_$_[1]" : ''));
-}
-sub _install_basic_post_routes {
-    my ( $r, $n ) = @_;
-    $r->post('/new')
-      ->to('board#new_post')
-      ->name("new_post$n");
-    $r->get('/showpost/:postid', [postid => $Digqr])
-      ->to('board#show_post')
-      ->name("show_post$n");
-}
-sub _install_edit_routes {
-    my $r = $_[0];
-    my $n = $_[1] ? "_$_[1]" : '';
-
-    # Post-Handling
-    _install_basic_post_routes($r, $n);
-    $r->get('/edit/:postid', [postid => $Digqr])
-      ->to('board#edit_post_form')
-      ->name("edit_post_form$n");
-    $r->post('/edit/:postid', [postid => $Digqr])
-      ->to('board#edit_post_do')
-      ->name("edit_post_do$n");
-    $r->get('/delete/:postid', [postid => $Digqr])
-      ->to('board#delete_post_confirm')
-      ->name("delete_post$n");
-    $r->post('/delete/:postid', [postid => $Digqr])
-      ->to('board#delete_post_do')
-      ->name("delete_post$n");
-    
-    # Upload-Handling
-    $r->get('/upload/:postid', [postid => $Digqr])
-      ->to('board#upload_form')
-      ->name("upload_form$n");
-    $r->post('/upload/:postid', [postid => $Digqr])
-      ->to('board#upload')
-      ->name("upload$n");
-    $r->get('/upload/delete/:uploadid', [uploadid => $Digqr])
-      ->to('board#deleteupload_confirm')
-      ->name("delete_upload_confirm$n");
-    $r->post('/upload/delete/:uploadid', [uploadid => $Digqr])
-      ->to('board#delete_upload_do')
-      ->name("delete_upload_do$n");
-}
-sub _install_switch_bridge {
-    my $n = $_[1];
-    my $r = $_[0]->bridge("/$n")->under(sub{$_[0]->stash(act => $n)})->name("switch_${n}_bridge");
-    _install_show_route($r, $_[1]);
-    return $r;
-}
-sub _install_routes_forum {
-    my $n = 'forum';
-    my $r = _install_switch_bridge($_[0], $n);
-    _install_edit_routes($r, $n);
-    my $c = $r->bridge('/cat/:catname', [catname => $Catqr])->name('forum_cat_bridge');
-    _install_show_route($c, 'forum_cat');
-    _install_edit_routes($c, 'forum_cat');
-    for ( [$r,$n], [$c,"${n}_cat"] ) {
-        my ( $r, $n ) = @$_;
-        my $k = $r->bridge('/comment')->name("${n}_comment_bridge");
-        _install_edit_routes($k, "${n}_comment");
-    }
-}
-sub _install_routes_pmsgs {
-    my $n = 'pmsgs';
-    my $r = _install_switch_bridge($_[0], $n)
-                ->bridge('/user/:username', [username => $Usrqr])
-                ->name("${n}_user_bridge");
-    _install_basic_post_routes($r, "_$n");
-}
-sub _install_routes_notes {
-    my $n = 'notes';
-    my $r = _install_switch_bridge($_[0], $n);
-    _install_edit_routes($r, $n);
-}
-
 sub _install_routes_auth {
     my $r = $_[0];
     # Anmeldehandling und Anmeldeprüfung
@@ -122,14 +44,34 @@ sub _install_routes_auth {
              ->name('login_check');
 }
 
-sub _install_routes_avatars {
-    my $p = $_[0]->bridge('/avatar')->name('avatars_bridge');
-    $p->get('/:username', [username => $Usrqr])
-      ->to('avatars#avatar_show')
-      ->name('avatar_show');
-    $p->post('/upload')
-      ->to('avatars#avatar_upload')
-      ->name('avatar_upload');
+sub _install_routes_forum {
+    my $n = 'forum';
+    my $r = _install_switch_bridge($_[0], $n);
+    _install_edit_routes($r, $n);
+    my $c = $r->bridge('/cat/:catname', [catname => $Catqr])
+              ->name('forum_cat_bridge');
+    _install_show_route($c, 'forum_cat');
+    _install_edit_routes($c, 'forum_cat');
+    for ( [$r,$n], [$c,"${n}_cat"] ) {
+        my ( $r, $n ) = @$_;
+        my $k = $r->bridge('/comment')
+                  ->name("${n}_comment_bridge");
+        _install_edit_routes($k, "${n}_comment");
+    }
+}
+
+sub _install_routes_pmsgs {
+    my $n = 'pmsgs';
+    my $r = _install_switch_bridge($_[0], $n)
+                ->bridge('/user/:username', [username => $Usrqr])
+                ->name("${n}_user_bridge");
+    _install_basic_post_routes($r, "_$n");
+}
+
+sub _install_routes_notes {
+    my $n = 'notes';
+    my $r = _install_switch_bridge($_[0], $n);
+    _install_edit_routes($r, $n);
 }
 
 sub _install_routes_options {
@@ -168,6 +110,82 @@ sub _install_routes_options {
     $o->post('/useradmin')
       ->to('options#useradmin')
       ->name('useradmin');
+}
+
+sub _install_routes_avatars {
+    my $p = $_[0]->bridge('/avatar')->name('avatars_bridge');
+    $p->get('/:username', [username => $Usrqr])
+      ->to('avatars#avatar_show')
+      ->name('avatar_show');
+    $p->post('/upload')
+      ->to('avatars#avatar_upload')
+      ->name('avatar_upload');
+}
+
+sub _install_switch_bridge {
+    my $n = $_[1];
+    my $r = $_[0]->bridge("/$n")
+                 ->under(sub{$_[0]->stash(act => $n)})
+                 ->name("switch_${n}_bridge");
+    _install_show_route($r, $_[1]);
+    return $r;
+}
+
+sub _install_show_route {
+    $_[0]->get('/show')
+         ->to('board#frontpage')
+         ->name('show'.($_[1] ? "_$_[1]" : ''));
+    $_[0]->post('/search')
+         ->to('board#frontpage')
+         ->name('search'.($_[1] ? "_$_[1]" : ''));
+}
+
+sub _install_basic_post_routes {
+    my ( $r, $n ) = @_;
+    $r->post('/new')
+      ->to('board#new_post')
+      ->name("new_post$n");
+    $r->get('/showpost/:postid', [postid => $Digqr])
+      ->to('board#show_post')
+      ->name("show_post$n");
+}
+sub _install_edit_routes {
+    my $r = $_[0];
+    my $n = $_[1] ? "_$_[1]" : '';
+
+    # post show and editing
+    _install_basic_post_routes($r, $n);
+    $r->get('/edit/:postid', [postid => $Digqr])
+      ->to('board#edit_post_form')
+      ->name("edit_post_form$n");
+    $r->post('/edit/:postid', [postid => $Digqr])
+      ->to('board#edit_post_do')
+      ->name("edit_post_do$n");
+    $r->get('/delete/:postid', [postid => $Digqr])
+      ->to('board#delete_post_confirm')
+      ->name("delete_post$n");
+    $r->post('/delete/:postid', [postid => $Digqr])
+      ->to('board#delete_post_do')
+      ->name("delete_post$n");
+
+    _install_upload_routes($r, $r);
+}
+
+sub _install_upload_routes {
+    my ( $r, $n ) = @_;
+    
+    $r->get('/upload/:postid', [postid => $Digqr])
+      ->to('board#upload_form')
+      ->name("upload_form$n");
+    $r->post('/upload/:postid', [postid => $Digqr])
+      ->to('board#upload')
+      ->name("upload$n");
+    $r->get('/upload/delete/:uploadid', [uploadid => $Digqr])
+      ->to('board#deleteupload_confirm')
+      ->name("delete_upload_confirm$n");
+    $r->post('/upload/delete/:uploadid', [uploadid => $Digqr])
+      ->to('board#delete_upload_do')
+      ->name("delete_upload_do$n");
 }
 
 1;
