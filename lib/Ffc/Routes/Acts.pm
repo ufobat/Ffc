83597eb8d7b719d54ea820e83409f2c8ec1cdcb9
Bedivere <peters_mops@arcor.de>
2014-05-11 14:05:48 +0200

    Routen gefixt, jetzt sieht das so aus, wie ich mir das denke

--- lib/Ffc/Routes/Acts.pm
+++ lib/Ffc/Routes/Acts.pm
@@ -12,7 +12,7 @@ sub _install_routes_forum {
     my $r = _install_switch_bridge($_[0], $n);
 
     # category management
-    my $c = $r->bridge('/cat/:catname', [catname => $Ffc::Catqr])
+    my $c = $r->bridge('/cat/:catname', catname => $Ffc::Catqr)
               ->name($n.'_cat_bridge');
     $n = 'forum_cat';
     _install_routes($c, $n);
@@ -23,7 +23,7 @@ sub _install_routes_pmsgs {
     my $r = _install_switch_bridge($_[0], $n);
 
     # user conversation management
-    my $u = $r->bridge('/user/:username', [username => $Ffc::Usrqr])
+    my $u = $r->bridge('/user/:username', username => $Ffc::Usrqr)
               ->name($n.'_user_bridge');
     $n = 'pmsgs_user';
     _install_routes($u, $n);
@@ -34,7 +34,7 @@ sub _install_routes_notes {
 }
 
 sub _install_switch_bridge {
-    my $n = $_[1] // 'forum';
+    my $n = $_[1];
     my $r = $_[0]->bridge("/$n")
                  ->under(sub{$_[0]->stash(act => $n)})
                  ->name("switch_${n}_bridge");
@@ -74,34 +74,34 @@ sub _install_display_fac {
 
 sub _install_upload_fac {
     my $n = ($_[1] // '').'_upload';
-    my $r = $_[0]->bridge('/upload/:postid', [postid => $Ffc::Digqr])
+    my $r = $_[0]->bridge('/upload/:postid', postid => $Ffc::Digqr)
                  ->name($n.'_fac_bridge');
 
     # upload a new file for an entry
-    $r->via('get')
+    $r->get('/form')
       ->to('board#upload_form')
       ->name($n.'_form');
-    $r->via('post')
+    $r->post('/do')
       ->to('board#upload')
       ->name($n.'_do');
 
     # handle single uploads
-    my $u = $r->bridge('/upload/:uploadid', [uploadid => $Ffc::Digqr])
+    my $u = $r->bridge('/file/:uploadid', uploadid => $Ffc::Digqr)
               ->name($n.'_bridge');
 
     # show the upload file
-    $u->via('get')
+    $u->get('/download')
       ->to('board#download')
       ->name($n.'_download');
 
     # delete an uploaded file from an entry
     $n = $n . '_delete';
-    my $d = $r->bridge('/delete')
+    my $d = $u->bridge('/delete')
               ->name($n.'_bridge');
-    $d->via('get')
-      ->to('board#deleteupload_confirm')
+    $d->get('/confirm')
+      ->to('board#delete_upload_confirm')
       ->name($n.'_confirm');
-    $d->via('post')
+    $d->post('/do')
       ->to('board#delete_upload_do')
       ->name($n.'_do');
 
@@ -110,7 +110,7 @@ sub _install_upload_fac {
 
 sub _install_comment_fac {
     my $n = $_[1] // '';
-    my $r = $_[0]->bridge('/comment/:postid', [postid => $Ffc::Digqr])
+    my $r = $_[0]->bridge('/comment/:postid', postid => $Ffc::Digqr)
                  ->name($n.'_comment_fac_bridge');
 
     # add a new comment for an entry
@@ -125,11 +125,6 @@ sub _install_comment_fac {
 sub _install_edit_fac {
     my ( $r, $n ) = @_;
 
-    # display an entry
-    $r->get('/show/:entryid', [entryid => $Ffc::Digqr])
-      ->to('board#show_entry')
-      ->name($n.'_show');
-
     # add a new entry
     $r->post('/new')
       ->to('board#new_entry')
@@ -137,26 +132,31 @@ sub _install_edit_fac {
 
     # handle single entries
     $n = $n . '_entry';
-    my $p = $r->bridge('/entry/:entryid', [entryid => $Ffc::Digqr])
+    my $p = $r->bridge('/entry/:entryid', entryid => $Ffc::Digqr)
               ->name($n.'_bridge');
+
+    # display an entry
+    $p->get('/show')
+      ->to('board#show_entry')
+      ->name($n.'_show');
 
     # edit a single entry
     my $e = $p->bridge('/edit')
               ->name($n.'_edit');
-    $e->via('get')
+    $e->get('/form')
       ->to('board#edit_entry_form')
       ->name($n.'_form');
-    $e->via('post')
+    $e->post('/save')
       ->to('board#edit_entry_save')
       ->name($n.'_save');
 
     # delete a single entry
     my $d = $p->bridge('/delete')
               ->name($n.'_delete');
-    $d->via('get')
+    $d->get('/confirm')
       ->to('board#delete_entry_confirm')
       ->name($n.'_delete_confirm');
-    $d->via('post')
+    $d->post('/do')
       ->to('board#delete_do')
       ->name($n.'_delete');
 
