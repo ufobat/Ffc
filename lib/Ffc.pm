2c338287a3dbfa37a46d4c26ed0c0842025c1dfe
Bedivere <peters_mops@arcor.de>
2014-06-06 11:51:19 +0200

    Extra-Datei für Routen weggeräumt

--- lib/Ffc.pm
+++ lib/Ffc.pm
@@ -19,7 +19,35 @@ sub startup {
     $app->plugin('Ffc::Plugin::Formats');
     $app->plugin('Ffc::Plugin::Posts');
     $app->helper(login_ok => sub { $_[0]->session->{user} ? 1 : 0 });
-    Ffc::Routes::install_routes($app);
+    _install_routes($app->routes);
+}
+
+sub _install_routes {
+    my $l = Ffc::Auth::install_routes($_[0]);
+    use Ffc::Options;
+    use Ffc::Auth;
+    use Ffc::Avatars;
+    use Ffc::Forum;
+    use Ffc::Pmsgs;
+    use Ffc::Notes;
+    Ffc::Avatars::install_routes($l);
+    Ffc::Options::install_routes($l);
+    Ffc::Forum::install_routes($l);
+    Ffc::Pmsgs::install_routes($l);
+    Ffc::Notes::install_routes($l);
+    _install_routes_helper($l);
+}
+
+sub _install_routes_helper {
+    my $l = $_[0];
+    # Standardseitenauslieferungen
+    $l->any('/')->to('forum#show')->name('show');
+    $l->any('/help' => sub { $_[0]->render(template => 'help' ) } )
+      ->name('help');
+    $l->get('/session' => sub { $_[0]->render( json => $_[0]->session() ) } )
+      ->name('sessiondata');
+    $l->get('/config' => sub { $_[0]->render( json => $_[0]->configdata() ) } )
+      ->name('configdata');
 }
 
 1;
