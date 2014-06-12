663bffd53cf86dc22426685e402ebf620f68aeb2
Bedivere <peters_mops@arcor.de>
2014-06-12 20:13:08 +0200

    Die kommentare brauchen wir nicht mehr, die Funktionen sind doch selbsterklärend

--- lib/Ffc/Notes.pm
+++ lib/Ffc/Notes.pm
@@ -2,217 +2,69 @@ package Ffc::Notes;
 use strict; use warnings; use utf8;
 use Mojo::Base 'Mojolicious::Controller';
 
-# Dieser Standardcontroller verwaltet eine bestimmte Art von Beiträgen
-# in der Datenbank und deren Anhänge. Dazu verwendet er das Plugin
-# Ffc::Plugin::Posts. Dieses Plugin kümmert sich um alle Feinheiten
-# der Beitrags- und Anhangsverwaltung. Das Konzept bei der Anwendung
-# ist dabei folgendes:
-# - Die Formulare werden über Stash-Variablen konfiguriert. Dabei
-#   werden Überschriften sowie Routen-Namen mit Parametern in speziellen
-#   vorgeschriebenen Variablen abgelegt.
-# - Stash-Variablen, welche Routen beschreiben, können fest verdrahtet
-#   oder als Array-Referenz übergeben werden, je nach dem, ob das Plugin
-#   diese gleich fertig übernehmen kann oder ob es selber noch Variablen
-#   einfügen muss.
-# - In den hier definierten Routen müssen bestimmte Parameternamen
-#   verwendet werden: "postid" für die ID eines Beitrages sowie "fileid" 
-#   für die ID eines Dateianhanges. Diese Parameter beziehen sich auf die
-#   entsprechenden "id"-Felder in der Datenbank.
-# - Das Plugin stellt Prozeduren zur Verfügung, welche die eigentliche
-#   Arbeit übernehmen.
-# - Die Arbeitsprozeduren bekommen als Funktionsparameter einen SQL-Bestandteil
-#   sowie eine Liste von Parametern mit. Der SQL-Bestandteil wird in die 
-#   WHERE-Klausel im SQL bei Datenbankabfragen eingebaut und die Parameter
-#   entsprechen den darin enthaltenen Platzhaltern.
-# - Über diese Funktionsparameter werden die Beiträge eingeschränkt. So kann
-#   hierrüber festgelegt werden, an wen ein Beitrag gerichtet ist und
-#   unter welchem Titel er zu finden ist.
-# - Das Plugin kümmert sich intern auch darum, dass Benutzer nur die 
-#   Beiträge bearbeiten können, deren Urheber sie auch sind. Welche Beiträge
-#   ein Benutzer sehen kann, dass muss über die Funktionsparameter 
-#   eingeschränkt werden.
-# - Seitenweiterschaltung bei Beitragslisten sowie die Filterfunktion
-#   im Menü wird ebenfalls von dem Plugin in die Auflistungen eingebaut.
-# - Werden Routenangaben weggelassen, steht die entsprechende Funktion
-#   im erzeugten Formular dann auch nicht zur Verfügung. Wenn sich das 
-#   Formular allerdings um genau diese Funktion dreht, dann gibt das
-#   natürlich einen Fehler.
-
-# Als erstes werden über die folgende Routine die notwendigen Routen definiert.
-# Diese Routine wird aus Ffc::Routes angesprochen und übernimmt eine Bridge,
-# bei welcher man bereits angemeldet ist und bei der die Anmeldung geprüft wird.
 sub install_routes { Ffc::Plugin::Posts::install_routes_posts($_[0], 'notes', '/notes') }
 
-# Im Folgenden werden die Standart-Einschränkungen ("WHERE"-Bestandteile) 
-# für die Abfrage von Beiträgen in der Datenbank festgelegt.
-# Where-Bestandteile dienen dazu, den Aktionsradius von Funktionen
-# innerhalb dieses Teils des Ffc einzuschränken, sind aber optional.
-
-# Where-Bestandteil zum Suchen von Beiträgen in der Datenbank
-# - Dieser SQL-Bestandteil benötigt das Prefix "p." für Feldnamen, 
-#   da hier mehere Tabellen beim "SELECT" gejoint werden
-# - Dieser Bestandteil wird zum anzeigen der Beitragsliste ($c->show) 
-#   sowie zum anzeigen des einzelnen Beitrags bei edit und delete verwendet
-# - Dieser Bestandteil wird in die Where-Clause eingebaut
-# - "?"-Parameter müssen beim Aufruf der entsprechenden Helper-Subs
-#   mit in der entsprechenden Reihenfolge übergeben werden
 sub where_select {
     return 
         'p."userfrom"=p."userto" AND p."userfrom"=?', 
         $_[0]->session->{userid};
 }
 
-# Where-Bestandteil zum Ändern von Beiträgen in der Datenbank
-# - Dieser SQL-Bestandteil darf keine Prefixe für Feldnamen enthalten,
-#   da "UPDATE" und "DELETE" sonst auffe Schnauze fallen
-# - Dieser Bestandteil wird beim Ausführen des Editieren- und Löschenvorganes
-#   in der Datenbank verwendet
-# - Dieser Bestandteil wird in die Where-Clause eingebaut
-# - "?"-Parameter müssen beim Aufruf der entsprechenden Helper-Subs
-#   mit in der entsprechenden Reihenfolge übergeben werden
 sub where_modify {
     return 
         '"userfrom"="userto" AND "userfrom"=?',
         $_[0]->session->{userid};
 }
 
-# Folgende Methode ermittelt eine letzte bekannte Beitrags-ID innerhalb
-# eines Zweiges. Anhand dieser ID wird bei neuen Beiträgen ermittelt,
-# ob eine Warnung angezeigt wird oder nicht. Gibt es keine Notwendigkeit
-# das zu prüfen, kann hier einfach "undef" zurück gegeben werden.
 sub lastseen { return }
 
-# Folgende Methode weißt den URL's im Template zusätzliche Parameter zu,
-# die für die fertige URL zu verwenden sind. Anders geht das irgendwie nicht.
 sub additional_params { return () }
 
-# Folge-Aktionen werden über Routennamen oder fertige Routen-URL's in
-# Stash-Variablen gespeichert. Bestimmte Aktionen werden im Verlauf der
-# Arbeit des Plugins "Posts" mit dynamischen Werten versehen und müssen
-# dementsprechend als Routenname übergeben werden. Dazu werden sie in
-# eine Arrayreferenz gepackt als ersten Eintrag und etwaige weitere
-# Parameter, welche dieser Route übergeben werden müssen, können
-# als weitere Parameter in dieser Arrayreferenz weiter hinten an
-# gehängt werden. Einige Routen stehen allerdings fest und können
-# vor dem Rendern der Templates schon fertig zur URL verbaut werden
-# (wegen Performance). Diese Routen können bereits fertig erstellt werden
-# an einer Stelle und werden als fertige URL's in die entsprechenden
-# Stash-Variablen geschrieben.
-
-# Folgende Funktionen stellen die Aktionen dar, die über die
-# Routen dieses Teils des Ffc Beitragslisten enthalten. Sie 
-# müssen, um mit den "Posts"-Plugin zu funktionieren, alle
-# vorhanden sein, da sie von diesem Plugin verwendet werden.
-# Jede Funktion kann optional über den Stash-Variablen "heading"
-# eine Überschrift festlegen, die an prominenter Stelle auf der
-# entsprechenden Seite angezeigt wird. Alles was über "dourl"
-# definiert wird, kann fest als URL übergeben werden, da hier
-# ein POST-Formular aufgebaut wird, welches keine weiteren Daten
-# benötigt. Alle weiteren Routenangaben werden als Array-Referenz
-# angelegt, und können somit weitere Parameter enthalten.
-
-# Diese Funktion bietet die normale Anzeigeseite an. Hier werden
-# Routen über Stash-Variablen definiert, welche auf dieser Seite 
-# zu den einzelnen Posts angeboten werden, sowie eine "dourl", über 
-# welche ein neuer Beitrag # erstellt wird. Außerdem wird die Route 
-# "pageurl" angeboten, über welche die Seitenschaltung abgewickelt wird.
 sub show {
     my $c = shift;
     $c->stash( 
         heading     => 'Persönliche Notizen', # Überschrift
         description => 'Hier können Texte und Dateianhänge für den eigenen internen Bedarf abgelegt werden.', # Kurzbeschreibung
     );
-    # Das folgende ist der Plugin-Helper-Aufruf, über den die Anzeige erfolgt.
-    # Dieser Aufruf bekommt einen "WHERE"-Bestandteil und die Liste der entsprechenden Parameter.
-    # Der Where-Bestandteil ist der für Anzeigen von Beiträgen.
     $c->show_posts();
 }
 
-# Hierüber wird die Filter aus dem Menü abgewickelt, was im 
-# vorliegenden Fall direkt an die entsprechende Helper-Routine 
-# weitergeleitet wird.
 sub query { $_[0]->query_posts }
 
-# Diese Funktion fügt neue Beiträge über die entsprechende
-# Helper-Sub hinzu, was direkt aus der Übersicht heraus funktioniert.
-# Als Argumente nimmt die Helper-Routine den Emfpänger (man selbst bei Notizen)
-# sowie die ID des Themas (Topic), unter dem der Eintrag erscheinen soll
-# (bleibt bei Notizen leer).
 sub add { $_[0]->add_post($_[0]->session->{userid}, undef) }
 
-# Diese Funktion erstellt ein Formular, über das Beiträge geändert werden können,
-# "dourl" definiert hierbei die Route, über die die Änderung gespeichert wird.
-# Bei Problemen bei der Änderung wird die Seite erneut angezeigt mit einer 
-# entsprechenden Fehlermeldung. "heading" gibt auch hier die Überschrift
-# an, falls gesetzt.
 sub edit_form {
     my $c = shift;
     $c->stash( heading => 'Persönliche Notiz ändern'); # Überschrift für das Eingabeformular
-    # Dieser Helper erzeugt ein Formular zum Ändern des entsprechenden
-    # Beitrages. Er bekommt den "WHERE"-Bestandteil zur Anzeige von Beiträgen
-    # sowie die Liste für dessen Parameter.
     $c->edit_post_form();
 }
 
-# Diese Funktion führt Änderungen an Beiträgen durch, die in "edit_form"
-# erstellt wurden. Sie leitet im Vorliegenden Fall einfach den Aufruf
-# weiter an die entsprechende Helper-Sub und bekommt dabei den
-# "WHERE"-Bestandteil und dessen Parameter für Datenmodifikationen mit.
 sub edit_do { $_[0]->edit_post_do() }
 
-# Diese Funktion leitet den Löschvorgang ein, indem sie nochmals um
-# Bestätigung nachfragt. Wird die Bestätigung verweigert, dann leitet der
-# zugehörige Helper auf "show" um, wird sie gewährt, wird die Route hinter
-# "dourl" umgesetzt. Sie verwendet den "WHERE"-Bestandteil für die Beitragsanzeige
-# mit passender Parameterliste.
 sub delete_check {
     my $c = shift;
     $c->stash( heading => 'Persönliche Notiz entfernen' ); # Überschrift für das Prüfungsformular
-    # Der folgende Helper erzeugt das Bestätigungsformular. Er bekommt
-    # einen "WHERE"-Bestandteil für Beitragsanzeigen sowie dessen
-    # Parameterliste übergeben.
     $c->delete_post_check();
 }
 
-# Diese Funktion führt das Löschen über die Helper-Sub durch und verwendet
-# dafür den "WHERE"-Bestandteil für Datenmodifikationen mit passender
-# Parameterliste. Anschließend leitet sie auf "show" um.
 sub delete_do { $_[0]->delete_post_do() }
 
-# Diese Funktion stellt ein Dateiupload-Fomular für einen Beitrag zur Verfügung.
-# Der Upload selber wird durch die Route hinter "dourl" umgesetzt. Sie verwendet
-# den "WHERE"-Bestandteil für die Beitragsanzeige mit passender Parameterliste.
 sub upload_form {
     my $c = shift;
     $c->stash( heading => 'Eine Datei zu einer persönlichen Notiz hochladen' ); # Überschrift für das Eingabeformular
-    # Dieser Helper erzeugt ein Formular zum Hochladen einer Datei zum entsprechenden
-    # Beitrag. Er bekommt den "WHERE"-Bestandteil zur Anzeige von Beiträgen
-    # sowie die Liste für dessen Parameter.
     $c->upload_post_form();
 }
 
-# Diese Funktion führt das Hochladen der gewählten Datei über die Helper-Sub 
-# durch und verwendet dafür den "WHERE"-Bestandteil für Datenmodifikationen 
-# mit passender Parameterliste. Anschließend leitet sie auf "show" um.
 sub upload_do { $_[0]->upload_post_do() }
 
-# Folgende Funktion erlaubt den Download von Dateianhängen zu Beiträgen
 sub download {  $_[0]->download_post() }
 
-# Die Folgende Funktion führt zu einer Bestätigungsseite, auf der noch
-# einmal nachgefragt wird, ob man einen bestimmten Dateianhang tatsächlich
-# komplett löschen möchte (im Dateisystem und in der Datenbank).
 sub delete_upload_check {
     my $c = shift;
     $c->stash( heading => 'Einen Dateianhang an einer Notiz entfernen' ); # Überschrift für das Bestätigungsformular
-    # Der folgende Helper erzeugt das Bestätigungsformular. Er bekommt
-    # einen "WHERE"-Bestandteil für Beitragsanzeigen sowie dessen
-    # Parameterliste übergeben.
     $c->delete_upload_post_check();
 }
 
-# Diese Funktion löscht einen Dateianhang nach der Bestätigung (aus vorhergehender
-# Funktion) komplett aus dem Dateisystem und aus der Datenbank.
 sub delete_upload_do { $_[0]->delete_upload_post_do() }
 
 1;
