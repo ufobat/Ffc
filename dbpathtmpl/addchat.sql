cddc3a63578bf4de226d957bda95989633772851
Bedivere <peters_mops@arcor.de>
2014-10-20 10:49:42 +0200

    ich brauche die defaults

--- dbpathtmpl/addchat.sql
+++ dbpathtmpl/addchat.sql
@@ -1,11 +1,8 @@
 ALTER TABLE "users" ADD COLUMN "inchat" tinyint(1) NOT NULL DEFAULT '0';
 ALTER TABLE "users" ADD COLUMN "lastchatid" integer NOT NULL DEFAULT '0';
-ALTER TABLE "users" ADD COLUMN "lastseenchat" timestamp;
-ALTER TABLE "users" ADD COLUMN "lastseenchatactive" timestamp;
+ALTER TABLE "users" ADD COLUMN "lastseenchat" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE "users" ADD COLUMN "lastseenchatactive" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE "users" ADD COLUMN "chatrefreshsecs" integer NOT NULL DEFAULT 60;
-
-UPDATE "users" SET "lastseenchat"=0;
-UPDATE "users" SET "lastseenchatactive"=0;
 
 CREATE TABLE "chat" (
     "id" integer PRIMARY KEY AUTOINCREMENT,
