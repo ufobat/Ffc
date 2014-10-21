09b23cede139b560f30bc22fab78e2fc64e4543d
Bedivere <peters_mops@arcor.de>
2014-10-21 18:00:46 +0200

    Bugfix 2

--- lib/Ffc/Plugin/Posts/Create.pm
+++ lib/Ffc/Plugin/Posts/Create.pm
@@ -10,8 +10,8 @@ sub _add_post {
         $c->set_error('Es wurde zu wenig Text eingegeben (min. 2 Zeichen)');
         return $c->show;
     }
-    my $controller = $c->stash('controller');
-    if ( $controller ne 'notes' and ( $userto or $topicid ) ) {
+    #my $controller = $c->stash('controller');
+    if ( $userto or $topicid ) {
         my $sql = 'SELECT'
             . ' CASE WHEN MAX(COALESCE(p.id,0))>MAX(COALESCE(l.lastseen,0)) THEN 1 ELSE 0 END'
             . ' FROM users u LEFT OUTER JOIN posts p ON p.userfrom<>? AND p.'
