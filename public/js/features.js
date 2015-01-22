57acd09ffc303d1b61adec3b25bc4d2c17c376a8
Bedivere <peters_mops@arcor.de>
2015-01-22 15:11:53 +0100

    utorefresh verbessert und gebugfixt

--- public/js/features.js
+++ public/js/features.js
@@ -1,10 +1,10 @@
 ffcdata.features = {};
 
 ffcdata.features.init = function(){
+    var mytitle = document.getElementsByTagName("title")[0].firstChild.data;
 
     // Aktualisierungszeitpunkt nach lokaler Uhrzeit in den Titel schreiben
     var set_titletime = function(){
-        var mytitle = document.getElementsByTagName("title")[0].firstChild.data;
         var mytime  = new Date();
         var myh = mytime.getHours(), mym = mytime.getMinutes();
         document.getElementsByTagName("title")[0].firstChild.data
@@ -16,10 +16,15 @@ ffcdata.features.init = function(){
     // Auto-Refresh einsetzen
     var set_autorefresh = function(){
         ffcdata.features.autorefresh_interval = window.setInterval(function(){
+            if ( document.hasFocus() 
+              && (document.getElementById('textinput')
+                && document.getElementById('textinput').value === '') ) {
+                return;
+            }
             ffcdata.utils.request('GET', ffcdata.counturl, null, function(res){ 
-                if ( res > 0 )
+                if ( res > 0 && res > ffcdata.lastcount ) 
                     location.reload();
-                else
+                else 
                     set_titletime();
             });
         }, ffcdata.autorefresh * 60000 );
@@ -27,12 +32,7 @@ ffcdata.features.init = function(){
     
     // Weitere Feature-Operationen starten
     set_titletime();
-    if ( ffcdata.autorefresh > 0
-      && !ffcdata.utils.is_disabled()
-      && !document.hasFocus() 
-      && (!document.getElementById('textinput') 
-        || document.getElementById('textinput').value === '') ) {
+    if ( ffcdata.autorefresh > 0 && !ffcdata.utils.is_disabled() )
         set_autorefresh();
-    }
 };
 
