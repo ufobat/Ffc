82623f64449ecec0fcb1dc941e92a7da58847a16
Bedivere <peters_mops@arcor.de>
2015-10-25 12:07:16 +0100

    Datenbankschema eingebaut

--- dbpathtmpl/db_schema.sql
+++ dbpathtmpl/db_schema.sql
@@ -29,6 +29,14 @@ CREATE TABLE "users" (
 CREATE INDEX "user_active_ix" ON "users"("active");
 CREATE INDEX "user_inchat_ix" ON "users"("inchat");
 
+CREATE TABLE "readlater" (
+    "userid" int(11) NOT NULL,
+    "postid" int(11) NOT NULL,
+    PRIMARY KEY ("userid", "postid")
+);
+CREATE INDEX "readlater_userid_ix" ON "readlater"("userid");
+CREATE INDEX "readlater_postid_ix" ON "readlater"("postid");
+
 CREATE TABLE "topics" (
   "id" integer PRIMARY KEY AUTOINCREMENT,
   "userfrom" int(11) NOT NULL,
