6806e0a04fb57a96fc5e98dc953193051fad0a34
Bedivere <peters_mops@arcor.de>
2015-10-29 23:32:01 +0100

    Huch, wo kommt denn das her

--- updatedb.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "hideemail" tinyint(1) NOT NULL DEFAULT 1;
-ALTER TABLE "users" ADD COLUMN "phone" varchar(50);
-ALTER TABLE "users" ADD COLUMN "birthdate" varchar(10);
-ALTER TABLE "users" ADD COLUMN "infos" varchar(1024);
