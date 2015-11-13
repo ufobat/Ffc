7a51a0b21af3434e6d9b071d65246b6c6d9aaf15
Bedivere <peters_mops@arcor.de>
2015-11-13 15:09:39 +0100

    Mailsend implementiert - ungetestet

--- script/mailsend.pl
+++ script/mailsend.pl
@@ -21,19 +21,23 @@ my $sender = 'admin@'.hostname();
         my $c = shift;
         $c->session->{userid} = $c->param('userid');
         $c->counting;
-        $c->render( text => join '=',
-            map { ( $c->stash($_.'count') // 0 ) }
-                qw(newmsgs newpost),
-        );
+        $c->render(json => {
+            newmsgs_count => $c->stash('newmsgs_count'),
+            newposts      => [
+                map  {;[@{$_}[2,3]]} 
+                grep {;$_->[10]}
+                    @{$c->stash('topics')},
+            ],
+        });
     };
-    get '/title' => sub {
+    get '/title'   => sub {
         $_[0]->render(text => $_[0]->configdata->{title});
     };
     get '/userids' => sub {
         $_[0]->render(json => $_[0]->dbh_selectall_arrayref( << 'EOSQL' ));
     SELECT u."name", u."email", u."id" 
     FROM "users" u
-    WHERE u."email" IS NOT NULL AND u."email"<>'' AND u."newsmail"=1 AND u."active"=1
+    WHERE u."email" IS NOT NULL AND u."email"<>'' AND u."active"=1
     ORDER BY UPPER("name"), "id"
 EOSQL
     };
@@ -44,50 +48,49 @@ my $title = $t->get_ok('/title')->tx->res->text   || 'Forum';
 my $users = $t->get_ok('/userids')->tx->res->json || [];
 
 for my $u ( @$users ) {
-    my $count_pmsgs = 0;
-    my $count_forum = 0;
-    if ( $t->get_ok("/$u->[2]") ) {
-        my $text = $t->tx->res->text;
-        if ( $text =~ m/(?<pmsgs>\d+)=(?<forum>\d+)/xmso ) {
-            $count_pmsgs = $+{pmsgs};
-            $count_forum = $+{forum};
-        }
+    my ( $username, $email, $uid ) = @$u;
+    my $cnt = 0; my @lines; my $cntp = 0;
+
+    my $data = $t->get_ok("/$uid")->tx->res->json;
+
+    if ( $data->{newmsgs_count} ) {
+        push @lines, "Private Nachrichten: $data->{newmsgs_count}\n";
+        $cnt += $data->{newmsgs_count};
+        say "Benutzer $username hat $cnt neue private Nachrichten erhalten.";
     }
-    if ( $count_pmsgs or $count_forum ) {
-        say "Neuigkeiten ($count_pmsgs, $count_forum) fuer $u->[0] ($u->[1], $u->[2]).";
+    for my $d ( @{$data->{newposts}} ) {
+        my $cntp += scalar @{$d->[1]};
+        push @lines, "$d->[0]: $d->[1]";
+    }
+    if ( $cntp ) {
+        say "Benutzer $username wird ueber $cntp neue Beitraege informiert.";
+    }
+    if ( $cnt + $cntp ) {
+        send_email($username, $email, \@lines);
+        say "Benutzer $username wurde per Email informiert.";
     }
     else {
-        say "Nichts neues fuer $u->[0] ($u->[1], $u->[2]).";
-        next;
+        say "Benutzer $username hat keine neuen Nachrichten erhalten.";
     }
+}
+
+sub send_email {
+    my ( $username, $email, $lines ) = @_;
     my $smtp = Net::SMTP->new($host) or die "Could not start to mail: $!";
     $smtp->mail($sender);
-    $smtp->to($u->[1]);
+    $smtp->to($email);
     $smtp->data();
     $smtp->datasend("Subject: Neue Nachrichten in $title\n");
-    $smtp->datasend("To: $u->[1]\n");
+    $smtp->datasend("To: $email\n");
     $smtp->datasend("\n");
-    $smtp->datasend("Hallo $u->[0],\n\nes ");
-    if ( $count_forum ) {
-        $count_forum == 1
-          ? $smtp->datasend("es wartet eine neue Forennachricht\n")
-          : $smtp->datasend("es warten $count_forum neue Forennachrichten\n");
-        if ( $count_pmsgs ) {
-            $count_pmsgs == 1
-              ? $smtp->datasend("und eine neue private Nachricht\n")
-              : $smtp->datasend("und $count_pmsgs neue private Nachrichten\n");
-        }
+    $smtp->datasend("Hallo $username,\n\n");
+    $smtp->datasend("es warten folgende neuen Nachrichten in $title auf dich:\n\n");
+    for my $l ( @$lines ) {
+        $smtp->datasend("    $l\n");
     }
-    else {
-        $count_pmsgs == 1
-          ? $smtp->datasend("es wartet eine neue private Nachricht\n")
-          : $smtp->datasend("es warten $count_pmsgs neue private Nachrichten\n");
-    }
-    $smtp->datasend("bei $title auf dich.\n\n");
-    $smtp->datasend("Viel Spass beim lesen.\n\n");
+    $smtp->datasend("\n\nViel Spass beim lesen.\n\n");
     $smtp->dataend();
     $smtp->quit or say "Could not send mail: $!";
-    say "Information über $count_forum Beitraege  und $count_pmsgs Nachrichten  an $u->[0] ($u->[1], $u->[2]) verschickt.";
 }
 
 done_testing( 2 + @$users );
