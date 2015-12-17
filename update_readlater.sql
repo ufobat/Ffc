9a949ecaf9faf9d5307c8b197e0eec9f1bf3a7bf
Bedivere <peters_mops@arcor.de>
2015-12-17 11:58:08 +0100

    Brauchen wir nicht mehr

--- update_readlater.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "readlater" (
-    "userid" int(11) NOT NULL, 
-    "postid" int(11) NOT NULL, 
-    PRIMARY KEY ("userid", "postid")
-);
-CREATE INDEX "readlater_userid_ix" ON "readlater"("userid");
-CREATE INDEX "readlater_postid_ix" ON "readlater"("postid");
