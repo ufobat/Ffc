f6417668d0ab80203c400eec7e08e06e76afdcd9
bedivere <peters_mops@arcor.de>
2016-02-17 15:04:46 +0100

    Datenbankschema vervollständigt und Init-Datenbank erstellt

--- db_schema/db_schema.sql
+++ db_schema/db_schema.sql
@@ -2,7 +2,7 @@
 /*** Konfiguration des Forums                                                                   ***/
 /**************************************************************************************************/
 CREATE TABLE "config" (
-    "key"   TEXT,
+    "key"   TEXT NOT NULL, -- Konfigurations-Schlüsselstring
     "value" TEXT
 );
 
@@ -16,11 +16,11 @@ CREATE INDEX "config_key_ix" ON "config"("key");
 /*** Benutzerdaten                                                                              ***/
 /**************************************************************************************************/
 CREATE TABLE "users" (
-    "is_active" INTEGER, -- Boolean
-    "name"      TEXT,
-    "color"     INTEGER, -- sprintf('%x', $_) ~ NULL erlaubt
-    "lastseen"  INTEGER, -- Unix-Timestamp
-    "is_inchat" INTEGER  -- Boolean
+    "is_active" INTEGER NOT NULL DEFAULT 1, -- Boolean
+    "name"      TEXT    NOT NULL,
+    "color"     INTEGER,                    -- sprintf('%x', $_)
+    "lastseen"  INTEGER,                    -- Unix-Timestamp
+    "is_inchat" INTEGER NOT NULL DEFAULT 0  -- Boolean
 );
 
 CREATE INDEX "users_is_active_ix" ON "users"("is_active");
@@ -31,10 +31,11 @@ CREATE INDEX "users_is_inchat_ix" ON "users"("is_inchat");
 /**************************************************************************************************/
 
 CREATE TABLE "users_config" (
-    "users_id" INTEGER, -- FK "users" rowid    ~ NOT NULL
-    "is_admin" INTEGER, -- Boolean             ~ 0|1
-    "password" TEXT,    -- Hash-Wert natürlich ~ NOT NULL
-    "bgcolor"  INTEGER  -- sprintf('%x', $_)   ~ NULL erlaubt
+    "users_id" INTEGER NOT NULL,
+    "is_admin" INTEGER NOT NULL DEFAULT 0, -- Boolean
+    "password" TEXT    NOT NULL,           -- Hash-Wert natürlich
+    "bgcolor"  INTEGER,                    -- sprintf('%x', $_)
+    FOREIGN KEY ("users_id") REFERENCES "users"(rowid)
 );
 
 CREATE INDEX "users_config_users_id_ix" ON "users_config"("users_id");
@@ -43,62 +44,74 @@ CREATE INDEX "users_config_users_id_ix" ON "users_config"("users_id");
 /*** Privatnachrichten-Management                                                               ***/
 /**************************************************************************************************/
 CREATE TABLE "users_2_users" (
-    "users_id"      INTEGER, -- FK "users".rowid
-    "users_from_id" INTEGER, -- FK "users".rowid
-    "posts_last_id" INTEGER  -- FK "posts".rowid
+    "users_id"      INTEGER NOT NULL,
+    "users_from_id" INTEGER NOT NULL,
+    "posts_last_id" INTEGER,
+    FOREIGN KEY ("users_id")      REFERENCES "users"(rowid),
+    FOREIGN KEY ("users_from_id") REFERENCES "users"(rowid),
+    FOREIGN KEY ("posts_last_id") REFERENCES "posts"(rowid) ON DELETE SET NULL
 );
 
-CREATE INDEX "users_2_users_users_id_ix" ON "users_2_users"("users_id");
+CREATE INDEX "users_2_users_users_id_ix"      ON "users_2_users"("users_id");
 CREATE INDEX "users_2_users_users_from_id_ix" ON "users_2_users"("users_from_id");
 
 /**************************************************************************************************/
 /*** Themen                                                                                     ***/
 /**************************************************************************************************/
 CREATE TABLE "topics" (
-    "title"         TEXT, -- NOT NULL
-    "posts_last_id" INTEGER, -- FK "posts".rowid, NULL erlaubt
-    "summary"       TEXT
+    "title"         TEXT NOT NULL,
+    "posts_last_id" INTEGER,
+    "summary"       TEXT,
+    FOREIGN KEY ("posts_last_id") REFERENCES "posts"(rowid) ON DELETE SET NULL
 );
 /**************************************************************************************************/
 /*** Benutzer-Themen-Management                                                                 ***/
 /**************************************************************************************************/
 CREATE TABLE "users_2_topics" (
-    "users_id"      INTEGER, -- FK "users".rowid, NOT NULL
-    "topics_id"     INTEGER, -- FK "topics".rowid, NOT NULL
-    "posts_last_id" INTEGER, -- FK "posts".rowid, NULL erlaubt
-    "pin"           INTEGER, -- Boolean
-    "ignore"        INTEGER  -- Boolean
+    "users_id"      INTEGER NOT NULL,
+    "topics_id"     INTEGER NOT NULL,
+    "posts_last_id" INTEGER,
+    "pin"           INTEGER NOT NULL DEFAULT 0, -- Boolean
+    "ignore"        INTEGER NOT NULL DEFAULT 0, -- Boolean
+    FOREIGN KEY ("users_id")      REFERENCES  "users"(rowid),
+    FOREIGN KEY ("topics_id")     REFERENCES "topics"(rowid),
+    FOREIGN KEY ("posts_last_id") REFERENCES  "posts"(rowid)  ON DELETE SET NULL
 );
 
 CREATE INDEX "users_2_topics_users_id_ix" ON "users_2_topics"("users_id");
-CREATE INDEX "users_2_topics_pin_ix" ON "users_2_topics"("pin");
-CREATE INDEX "users_2_topics_ignore_ix" ON "users_2_topics"("ignore");
+CREATE INDEX "users_2_topics_pin_ix"      ON "users_2_topics"("pin");
+CREATE INDEX "users_2_topics_ignore_ix"   ON "users_2_topics"("ignore");
 
 /**************************************************************************************************/
 /*** Beiträge                                                                                   ***/
 /**************************************************************************************************/
 CREATE TABLE "posts" (
-    "users_from_id"   INTEGER, -- FK "users".rowid  ~ NOT NULL
-    "users_to_id"     INTEGER, -- FK "users".rowid, ~ Privatnachricht,   sonst NULL
-    "topics_id"       INTEGER, -- FK "topics".rowid ~ Forenbeitrag,      sonst NULL
-    "parent_posts_id" INTEGER, -- FK "posts".rowid  ~ Beitragskommentar, sonst NULL
-    "create_time"     INTEGER, -- Unix-Timestamp    ~ NOT NULL, Default current_timestamp
-    "plain_text"      TEXT,    -- Eingegebener Text ohne Formatierungsumwandlungen, NOT NULL
-    "cache"           TEXT     -- Formatierter Text, NOT NULL
+    "users_from_id"   INTEGER NOT NULL,
+    "users_to_id"     INTEGER,                                    -- Privatnachricht
+    "topics_id"       INTEGER,                                    -- Forenbeitrag
+    "parent_posts_id" INTEGER,                                    -- Beitragskommentar
+    "create_time"     INTEGER NOT NULL DEFAULT current_timestamp, -- Unix-Timestamp
+    "plain_text"      TEXT    NOT NULL,                           -- Eingegebener Text ohne Formatierungsumwandlungen
+    "cache"           TEXT    NOT NULL,                           -- Formatierter Text
+    FOREIGN KEY ("users_from_id")   REFERENCES  "users"(rowid)
+    FOREIGN KEY ("users_to_id")     REFERENCES  "users"(rowid) ON DELETE SET NULL,
+    FOREIGN KEY ("topics_id")       REFERENCES "topics"(rowid) ON DELETE SET NULL,
+    FOREIGN KEY ("parent_posts_id") REFERENCES  "posts"(rowid) ON DELETE SET NULL
 );
 
-CREATE INDEX "posts_users_from_id_ix" ON "posts"("users_from_id");
-CREATE INDEX "posts_users_to_id_ix" ON "posts"("users_to_id");
-CREATE INDEX "posts_topics_id_ix" ON "posts"("topics_id");
+CREATE INDEX "posts_users_from_id_ix"   ON "posts"("users_from_id");
+CREATE INDEX "posts_users_to_id_ix"     ON "posts"("users_to_id");
+CREATE INDEX "posts_topics_id_ix"       ON "posts"("topics_id");
 CREATE INDEX "posts_parent_posts_id_ix" ON "posts"("parents_posts_id");
-CREATE INDEX "posts_create_time_ix" ON "posts"("create_time");
+CREATE INDEX "posts_create_time_ix"     ON "posts"("create_time");
 
 /**************************************************************************************************/
 /*** Beitragsanhänge                                                                            ***/
 /**************************************************************************************************/
 CREATE TABLE "attachements" (
-    "posts_id" INTEGER, -- FK "posts".rowid, NOT NULL
-    "filename" TEXT     -- NOT NULL
+    "posts_id" INTEGER NOT NULL,
+    "filename" TEXT    NOT NULL,
+    FOREIGN KEY ("posts_id") REFERENCES "posts"(rowid) ON DELETE SET NULL
 );
 
 CREATE INDEX "attachements_posts_id_ix" ON "attachements"("posts_id");
@@ -107,8 +120,10 @@ CREATE INDEX "attachements_posts_id_ix" ON "attachements"("posts_id");
 /*** Chatkomponente                                                                             ***/
 /**************************************************************************************************/
 CREATE TABLE "chat" (
-    "user_from_id" INTEGER, -- FK "users".rowid
-    "textdata_id"  INTEGER, -- FK "posts".rowid
+    "users_from_id" INTEGER NOT NULL,
+    "posts_id"      INTEGER NOT NULL,
+    FOREIGN KEY ("users_from_id") REFERENCES "users"(rowid),
+    FOREIGN KEY ("posts_id")      REFERENCES "posts"(rowid)    
 );
 
 
