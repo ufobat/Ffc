2cf88620454181baf521a9da88b1c1b864705db5
Bedivere <peters_mops@arcor.de>
2016-08-18 18:22:48 +0200

    Aufräumen - Upload-Plugin

--- lib/Ffc/Plugin/Uploads.pm
+++ lib/Ffc/Plugin/Uploads.pm
@@ -3,10 +3,10 @@ use 5.18.0;
 use strict; use warnings; use utf8;
 use Mojo::Base 'Mojolicious::Plugin';
 use Mojolicious::Types;
-use File::Spec::Functions qw(catfile);
+use File::Spec::Functions 'catfile';
 
-our $Types = Mojolicious::Types->new()->mapping;
-
+###############################################################################
+# Helper-Registration für das Plugin
 sub register {
     my ( $self, $app ) = @_;
     $app->helper( file_upload => \&_file_upload );
@@ -15,40 +15,52 @@ sub register {
     return $self;
 }
 
+###############################################################################
+# Bestimmte Dateiendungen können als Inline dargestellt werden im HTML (Bilder und Multimedia zum Beispiel)
 sub _is_inline {
     $_[1] =~ m~\A(?:image|video|audio)|\*/(?:jpe?g|bmp|gif|png|mkv|avi|divx|ogg|ogv|mp3|flac|wav)\z~xmsio;
 }
 
+###############################################################################
+# Bestimmte Dateiendungen zählen als Bilder
 sub _is_image {
     $_[1] =~ m~\Aimage|\*/(?:jp[eg]|bmp|gif|png)\z~xmsio;
 }
 
+###############################################################################
+# Den gesammelten File-Upload durchführen
 sub _file_upload {
     my @p = (@_);
-    my $i = 0;
+    # Eine Liste der Upload-Parameter erzeugen (benötigt für mehrere Downloads in einem Rutsch)
     my $files = $p[0]->every_param($p[1]);
     unless ( @$files ) {
         $p[0]->set_error_f("Kein $p[3] angegeben.");
         return;
     }
+    my $i = 0;
     my @rets;
+    # Jeweils eine Datei hochladen
     for my $file ( @$files ) {
         my @ret = _single_file_upload($file, @p);
-        unless ( @ret ) {
-            if ( $p[2] == 1 ) { last }
-            else              { next }
-        }
+        @ret or $p[2] == 1 ? last : next;
         push @rets, \@ret;
         $i++;
-        last if $p[2] and $i >= $p[2];
+        $p[2] and $i >= $p[2] and last;
     }
-    if   ( not @rets            ) { return }
-    if   ( $p[2] and $p[2] == 1 ) { return @{$rets[0]} }
-    else                          { return @rets }
+    # Es haben keine Uploads stattgefunden
+    if ( not @rets ) { return }
+    # Es hat genau ein Upload stattgefunden
+    if ( $p[2] and $p[2] == 1 ) { return @{$rets[0]} }
+    # Alle stattgefundenen Uploads für diesen Fall
+    return @rets;
 }
 
+###############################################################################
+# Eine einzelne Datei hochladen über die Mojolicious-Mechanismen
 sub _single_file_upload {
     my ( $file, $c, $param, $fnum, $name, $min_s, $max_s, $min_l, $max_l, $filenamesub ) = @_;
+
+    # Prüfen der Dateiparameter
     unless ( $file ) {
         $c->set_error_f("Kein $name angegeben.");
         return;
@@ -90,22 +102,25 @@ sub _single_file_upload {
         $c->set_error_f('Dateiname darf weder ".." noch "/" enthalten.');
         return;
     }
-
     my $filetype = $filename =~ m/\.(\w+)\z/ximso ? $1 : '';
+
+    # Content-Type-Angabe aus der Types-Liste von Mojolicious, falls vorhanden, hernehmen,
+    # oder eben einen Fallback auswählen
+    my $Types = Mojolicious::Types->new()->mapping;
     my $content_type = $filetype 
         ? ( exists $Types->{$filetype} ? $Types->{$filetype}->[0] : "*/$filetype" )
         : '*/*';
 
+    # Ablagepfad über eine übergebene Funktionsreferenz ermitteln
     my $path = $filenamesub->($c, $filename, $filetype, $content_type);
-    return unless $path;
+    $path or return;
 
+    # Upload durchführen und Upload-Informationen zurück liefern
     unless ( $file->move_to(catfile(@{$c->datapath}, @$path)) ) {
         $c->set_error_f("Dateiupload für das $name fehlgeschlagen.");
         return;
     }
-
     return $path->[-1], $filetype, $content_type;
 }
 
 1;
-
